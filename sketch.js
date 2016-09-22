49bef8e79d148390d471fbb14a29965413ad0d93
thijvb <thijsvb@gmail.com>
2016-09-22 19:20:40 +0200

    initial commit

---
+++ sketch.js
@@ -0,0 +1,11 @@
+var textfield;
+var submit;
+var test;
+
+void setup(){
+  noCanvas();
+
+  var textfield = createInput("Kikker sok rappoflappo");
+  var submit = createButton("Verbeter");
+  var test = createButton("Voorbeeld");
+}
