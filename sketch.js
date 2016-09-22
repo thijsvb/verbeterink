4a4427945fe950399935094d1bbeb05151b81ba2
thijvb <thijsvb@gmail.com>
2016-09-22 19:25:57 +0200

    padding!

--- sketch.js
+++ sketch.js
@@ -6,6 +6,9 @@ function setup() {
   noCanvas();
 
   var textfield = createInput("Kikker sok rappoflappo");
+  createP('');
   var submit = createButton("Verbeter");
+  submit.style("padding", "12px");
   var test = createButton("Voorbeeld");
+  test.style("padding", "12px");
 }
